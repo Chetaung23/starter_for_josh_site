30d4dcac5a6f3d4f3011798faec4403c72448a60
Chetaung23 <chetaung23@gmail.com>
2024-04-04 16:59:23 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -21,7 +21,10 @@
             <div class="row">
 
 <div class="col-md-4">
-<img class="img-fluid" src ="<?php the_field('articleimage');?>" alt="woman">
+<?php
+$image = get_field('articleimage');
+?>
+<img class="img-fluid" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
 <h4 class="article-title"><?php the_field('articletitle'); ?></h4>
 <p class="date" ><?php the_field('articledate'); ?></p>
 <p class="article-p"><?php the_field('articleexcerpt'); ?> </p>
@@ -29,24 +32,26 @@
 </div>
 
 <div class="col-md-4">
-  <img class="img-fluid" src="http://170.187.231.66/~mesh3/wp-content/themes/starter_for_josh_site/images/port2.jpg" alt="woman">
-  <h4 class="article-title"> A PORTRAIT</h4>
-  <p class="date"> Friday, November 10, 2030</p>
-  <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
-  </p>
-      <a class="readmore" href="#">CONTINUE READING </a>  
-  </div>
+<?php
+$image = get_field('articleimage_middle');
+?>
+<img class="img-fluid" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+<h4 class="article-title"><?php the_field('articletitle_middle'); ?></h4>
+<p class="date" ><?php the_field('articledate_middle'); ?></p>
+<p class="article-p"><?php the_field('articleexcerpt_middle'); ?> </p>
+<a href="<?php the_field('readmore_middle'); ?>" class="readmore">CONTINUE READING</a>  
+</div>
 
-  <div class="col-md-4">
-    <img class="img-fluid" src="http://170.187.231.66/~mesh3/wp-content/themes/starter_for_josh_site/images/port3.jpg" alt="woman">
-    <h4 class="article-title"> A PORTRAIT</h4>
-    <p class="date"> Friday, November 10, 2030</p>
-    <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.
-    </p>
-        <a class="readmore" href="#">CONTINUE READING </a>  
-    </div>
-
-
+<div class="col-md-4">
+<?php
+$image = get_field('articleimage_right');
+?>
+<img class="img-fluid" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+<h4 class="article-title"><?php the_field('articletitle_right'); ?></h4>
+<p class="date" ><?php the_field('articledate_right'); ?></p>
+<p class="article-p"><?php the_field('articleexcerpt_right'); ?> </p>
+<a href="<?php the_field('readmore_right'); ?>" class="readmore">CONTINUE READING</a>  
+</div>
 
             </div> <!-- row -->
         </div> <!-- container -->
